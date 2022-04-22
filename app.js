62d021a74df00703934646513667e1b5930cd2a5
roger10e <rogertenny@gmail.com>
2022-04-22 10:12:53 -0500

    added the first question asking for users name

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
-const fs = require('fs');
+const inquirer = require('inquirer');
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'what is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
 
-const generatePage = require('./src/page-template');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
 
 
-const [name, github] = profileDataArgs;
+// const pageHTML = generatePage(name.github);
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw err;
+// fs.writeFile('index.html', generatePage(name, github), err => {
+//     if (err) throw err;
     
-    console.log('Portfolio complete! Check out index.html to see the output!');
-});
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+// });
